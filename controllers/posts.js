ca8ae43674b96aee2287165bd9d8fdaeaf5a0e90
h-yung <helen.yung.h@gmail.com>
2022-10-15 01:21:33 -0400

    fixed logged-in header

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,7 +15,7 @@ module.exports = {
     }
   },
   getOtherUserProfile: async (req, res) => {
-    if (req.params.userid === req.user.id){
+    if (req.params.userid === req.user.id) {
       //go to main profile if own user
       try {
         const posts = await Post.find({ user: req.user.id });
@@ -25,7 +25,7 @@ module.exports = {
       }
     } else {
       try {
-        const person = await User.findById(req.params.userid)
+        const person = await User.findById(req.params.userid);
         const posts = await Post.find({ user: req.params.userid });
         res.render("profileOther.ejs", { posts: posts, user: person }); //user is not passing for some reason
       } catch (err) {
@@ -35,19 +35,21 @@ module.exports = {
   },
   getFeed: async (req, res) => {
     try {
-      const posts = await Post.find().sort({ createdAt: "desc" }) 
-      //.lean() strips down some extras but also meant the conditional for usersWhoLiked below did not work. So removed here. 
-      
+      const person = await User.findById(req.user.id);
+      console.log(person.username);
+      const posts = await Post.find().sort({ createdAt: "desc" });
+      //.lean() strips down some extras but also meant the conditional for usersWhoLiked below did not work. So removed here.
+
       //tacking on a like boolean outside the db
-      for (const post of posts){
-        if (post.usersWhoLiked.includes(req.user.id)){
+      for (const post of posts) {
+        if (post.usersWhoLiked.includes(req.user.id)) {
           post.likedByViewer = true;
         }
         //below not working - why this one continues to fail is unknown
         // let chosenPost = await Post.findOne({_id: req.params.id, usersWhoLiked: req.user.id })
         // console.log(`chosen post: ${chosenPost}`)
       }
-      res.render("feed.ejs", { posts: posts });
+      res.render("feed.ejs", { posts: posts, user: person });
     } catch (err) {
       console.log(err);
     }
@@ -56,31 +58,33 @@ module.exports = {
     try {
       const post = await Post.findById(req.params.id);
       const author = await User.findById(post.user);
-      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" })
+      const comments = await Comment.find({ post: req.params.id }).sort({
+        createdAt: "desc",
+      });
       // .lean();
       //got rid of lean again bc I need the likedByViewer logic to work
 
       //prob not the most efficient approach
       //essentially tacking on the username outside the db
       //including likes
-      for (const comment of comments){
+      for (const comment of comments) {
         const writer = await User.findById(comment.user);
         comment.writer = writer.userName;
-        if (comment.usersWhoLiked.includes(req.user.id)){
+        if (comment.usersWhoLiked.includes(req.user.id)) {
           comment.likedByViewer = true;
         }
       }
 
       //if req.user has liked this post, return a mark
-      const likedByViewer = post.usersWhoLiked.includes(req.user.id)
+      const likedByViewer = post.usersWhoLiked.includes(req.user.id);
       //returns a boolean
 
-      res.render("post.ejs", { 
-        post: post, 
+      res.render("post.ejs", {
+        post: post,
         author: author,
-        user: req.user, 
+        user: req.user,
         likedByViewer,
-        comments: comments 
+        comments: comments,
       });
     } catch (err) {
       console.log(err);
@@ -98,7 +102,7 @@ module.exports = {
         caption: req.body.caption,
         likes: 0,
         user: req.user.id,
-        usersWhoLiked: [] //needs a default, else disaster
+        usersWhoLiked: [], //needs a default, else disaster
       });
       console.log("Post has been added!");
       res.redirect("/profile");
@@ -107,33 +111,33 @@ module.exports = {
     }
   },
   likePostFromFeed: async (req, res) => {
-  //essentially the same as likePost but redirects back to feed
+    //essentially the same as likePost but redirects back to feed
     try {
       //check if userid is in the array for that post, = already liked it
-      let chosenPost = await Post.findOne(
-        {_id: req.params.id, usersWhoLiked: req.user.id });
-      if (chosenPost){
+      let chosenPost = await Post.findOne({
+        _id: req.params.id,
+        usersWhoLiked: req.user.id,
+      });
+      if (chosenPost) {
         await Post.findOneAndUpdate(
           { _id: req.params.id },
           {
             $inc: { likes: -1 },
-            $pullAll: { 'usersWhoLiked': [req.user.id] } 
+            $pullAll: { usersWhoLiked: [req.user.id] },
           }
         );
         console.log("Likes-1 and user from array");
-        
       } else {
         await Post.findOneAndUpdate(
           { _id: req.params.id },
           {
             $inc: { likes: 1 },
-            $addToSet: { 'usersWhoLiked': req.user.id } 
+            $addToSet: { usersWhoLiked: req.user.id },
           }
         );
         console.log("Likes +1");
       }
       res.redirect(`/feed`);
-
     } catch (err) {
       console.log(err);
     }
@@ -141,30 +145,30 @@ module.exports = {
   likePost: async (req, res) => {
     try {
       //check if userid is in the array for that post, = already liked it
-      let chosenPost = await Post.findOne(
-        {_id: req.params.id, usersWhoLiked: req.user.id });
-      if (chosenPost){
+      let chosenPost = await Post.findOne({
+        _id: req.params.id,
+        usersWhoLiked: req.user.id,
+      });
+      if (chosenPost) {
         await Post.findOneAndUpdate(
           { _id: req.params.id },
           {
             $inc: { likes: -1 },
-            $pullAll: { 'usersWhoLiked': [req.user.id] } 
+            $pullAll: { usersWhoLiked: [req.user.id] },
           }
         );
         console.log("Likes-1 and user from array");
-        
       } else {
         await Post.findOneAndUpdate(
           { _id: req.params.id },
           {
             $inc: { likes: 1 },
-            $addToSet: { 'usersWhoLiked': req.user.id } 
+            $addToSet: { usersWhoLiked: req.user.id },
           }
         );
         console.log("Likes +1");
       }
       res.redirect(`/post/${req.params.id}`);
-
     } catch (err) {
       console.log(err);
     }
